3402bd0449c99c753b95de356bc70860da39436e
FernandoJuk <fernandojukcunha@gmail.com>
2022-06-07 19:54:25 -0300

    Inicial

---
+++ app/src/main/java/com/example/aula2_topicos_avancados/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aula2_topicos_avancados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //fernando
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
