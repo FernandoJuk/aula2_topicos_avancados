f40b2d70bba32e012d9af82af24d5c26ee3adfc8
FERNANDO JUK <92902010+FernandoJuk@users.noreply.github.com>
2022-06-07 20:38:42 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/aula2_topicos_avancados/MainActivity.kt
+++ app/src/main/java/com/example/aula2_topicos_avancados/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.aula2_topicos_avancados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//teste
 class MainActivity : AppCompatActivity() {
     //Alteração versão 1.1
     override fun onCreate(savedInstanceState: Bundle?) {
